c8696cfb1a86bdfc8ae23d5eabcc1b5f3bf573e2
Samuel Kim <samjkim455@gmail.com>
2025-05-22 14:23:23 -0400

    updated variable names to be more meaningful

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const userInput = prompt('Enter a list of comma-separated froyo flavors');
-const makeClean = (str) => {
-  let arr = str.split(',');
-  let obj = {};
-  for (let i = 0; i < arr.length; i++) {
-    let currentOrder = arr[i];
-    if (currentOrder in obj) obj[currentOrder]++;
-    else obj[currentOrder] = 1;
+const flavorCounter = (initialOrder) => {
+  let orders = initialOrder.split(',');
+  let finalCounter = {};
+  for (let i = 0; i < orders.length; i++) {
+    let currentOrder = orders[i];
+    if (currentOrder in finalCounter) finalCounter[currentOrder]++;
+    else finalCounter[currentOrder] = 1;
   }
-  return obj;
+  return finalCounter;
 }
-console.log(makeClean(userInput));
+console.log(flavorCounter(userInput));
